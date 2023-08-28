47c9eae186297f9ad0ede0340f375a22c4d3a7ba
gagikharutyunyan12 <gagikharutyunyan189@gmail.com>
2023-08-28 00:47:46 -0700

    add background script

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,35 @@
-// TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-})
+const tabStates = {};
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "toggleScript") {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            const tabId = activeTab.id;
+
+            if (!tabId) {
+                console.error("error");
+                sendResponse({ success: false });
+                return;
+            }
+
+            if (!tabStates[tabId]) {
+                tabStates[tabId] = { running: false, intervalId: null };
+            }
+
+            const state = tabStates[tabId];
+            state.running = !state.running;
+
+            if (state.running) {
+                state.intervalId = setInterval(() => {
+                    console.log("ok", tabId);
+                }, 1000);
+            } else {
+                clearInterval(state.intervalId);
+            }
+
+            const response = { success: true };
+            sendResponse(response);
+        });
+    }
+    return true;
+});
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './popup.css'
-const App: React.FC<{}> = () => {
-  return (
-    <div>
-      test
-    </div>
-  )
-}
+import React, { FC, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Popup: FC<{}> = () => {
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+    const [isDisabled, setIsDisabled] = useState<boolean>(true);
+    const [currentTabId, setCurrentTabId] = useState<number | null>(null);
+
+    useEffect(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            const url = activeTab.url;
+
+            if (url && url.startsWith("https://github.com/")) {
+                setIsDisabled(false);
+            }
+        });
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            const tabId = activeTab.id;
+
+            chrome.storage.local.get("tabStates", (result) => {
+                const tabStates = result.tabStates || {};
+                const state = tabStates[tabId] || {};
+                setIsRunning(state.running || false);
+            });
+        });
+    }, []);
+
+    const handleToggleScript = () => {
+        const newScriptState = !isRunning;
+        setIsRunning(newScriptState);
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            if (activeTab) {
+                const tabId = activeTab.id;
+
+                chrome.runtime.sendMessage({ action: "toggleScript", data: newScriptState, tabId }, () => {
+                    chrome.storage.local.get("tabStates", (result) => {
+                        const tabStates = result.tabStates || {};
+                        tabStates[tabId] = { running: newScriptState };
+                        chrome.storage.local.set({ tabStates });
+                    });
+                });
+            }
+        });
+    };
+
+    return (
+        <div>
+            <h1>Extension Popup</h1>
+            <button onClick={handleToggleScript} disabled={isDisabled}>
+                {isRunning ? "Stop Script" : "Start Script"}
+            </button>
+        </div>
+    );
+};
+
 
 const root = document.createElement('div')
 document.body.appendChild(root)
-ReactDOM.createRoot(root).render(<App />)
+ReactDOM.createRoot(root).render(<Popup />)
